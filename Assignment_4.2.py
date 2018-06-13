066fe3fc5220ac55da1adf1d184bed6adcca56b5
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 11:53:09 +0530

    Statistics Assignment 4.2

---
+++ Assignment_4.2.py
@@ -0,0 +1,44 @@
+Problem Statement: 4.2
+
+Find the variance for the following set of data representing trees in California (heights in
+feet):   3, 21, 98, 203, 17, 9
+                                                                                  
+                                                                                  
+I Step : Add all numbers
+3 + 21 + 98 + 203 + 17 + 9 = 351
+
+
+II Step : Square your result from I step and and divide by the number of items (6).
+
+351 × 351 = 123,201
+
+123,201 / 6 = 20,533.5
+
+Consider 'A' = 20,533.5
+
+III Step : Now consider all the numbers from I Step, and square them seperately as below 
+and Add those numbers together after squaring:
+
+3 × 3 + 21 × 21 + 98 × 98 + 203 × 203 + 17 × 17 + 9 × 9
+
+9 + 441 + 9604 + 41209 + 289 + 81 = 51,633
+
+IV Step : Subtract the amount in 'A' (II Step) from the amount in III Step.
+
+51,633 – 20,533.5 = 31,099.5
+
+Consider 'B'= 31,099.5
+
+V Step : Subtract 1 from the number of items in your data set*.
+
+6 – 1 = 5
+
+IV Step : Divide the number in IV Step by the number in V Step.
+
+This gives the variance:
+    
+31,099.5/5 = 6,219.9
+
+
+Variance = 6,219.9
+                                                                                  
